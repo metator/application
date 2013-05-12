3f61058f52951a66ff8a486e189b3cb64ac9ee44
josh <josh.ribakoff@gmail.com>
2013-05-12 01:23:46 -0400

    should be able to set produce name & price

--- application/Product.php
+++ application/Product.php
@@ -3,13 +3,29 @@ class Product
 {
     protected $name;
 
-    function __construct($params)
+    function __construct($params=array())
     {
-        $this->name = $params['name'];
+        $this->name = isset($params['name']) ? $params['name'] : '';
+        $this->price = isset($params['price']) ? $params['price'] : '';
     }
 
     function name()
     {
         return $this->name;
+    }
+
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    function price()
+    {
+        return $this->price;
+    }
+
+    function setPrice($price)
+    {
+        $this->price = $price;
     }
 }
--- application/ProductTest.php
+++ application/ProductTest.php
@@ -1,9 +1,29 @@
 <?php
 class ProductTest extends PHPUnit_Framework_TestCase
 {
-    function testShouldBeAbleToSetProductNameThroughConstructor()
+    function testShouldSetNameThroughConstructor()
     {
         $product = new Product(array('name'=>'widget'));
-        $this->assertEquals('widget',$product->name(), 'should be able to set name through constructor');
+        $this->assertEquals('widget',$product->name(), 'should set name through constructor');
+    }
+
+    function testShouldSetNameThroughSetter()
+    {
+        $product = new Product;
+        $product->setName('widget');
+        $this->assertEquals('widget',$product->name(), 'should set name through setter');
+    }
+
+    function testShouldSetPriceThroughConstructor()
+    {
+        $product = new Product(array('price'=>5.15));
+        $this->assertEquals(5.15,$product->price(), 'should set price through constructor');
+    }
+
+    function testShouldSetPriceThroughSetter()
+    {
+        $product = new Product;
+        $product->setPrice(5.15);
+        $this->assertEquals(5.15,$product->price(), 'should set price through setter');
     }
 }
