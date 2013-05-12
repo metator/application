d8d6e670c0c7ce5caedf0080b5d735b1bae2053c
josh <josh.ribakoff@gmail.com>
2013-05-12 12:12:40 -0400

    Added bootstrap CSS boilerplate

--- application/controllers/ProductController.php
+++ application/controllers/ProductController.php
@@ -1,14 +1,11 @@
 <?php
-class IndexController extends Zend_Controller_Action
+class ProductController extends Zend_Controller_Action
 {
 
-    function indexAction()
+    function editAction()
     {
         $this->view->headLink()->appendStylesheet('/bootstrap/css/bootstrap.css');
         $this->view->headScript()->appendFile('/js/jquery.js');
         $this->view->headScript()->appendFile('/bootstrap/js/bootstrap.js');
-
-        $this->render('index','splash');
-        $this->render('product/list','default',true);
     }
 }
