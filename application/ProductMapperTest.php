56919660761a1c68f36487b0b6c8def7495278b1
josh <josh.ribakoff@gmail.com>
2013-05-12 15:29:11 -0400

    When saving Product ensure options for it's Attribute's are saved.

--- application/ProductMapperTest.php
+++ application/ProductMapperTest.php
@@ -49,19 +49,21 @@ class ProductMapperTest extends PHPUnit_Framework_TestCase
 
     function testShouldSaveAttributeOptions()
     {
-        return $this->markTestIncomplete();
-
         $product = new Product(array('name'=>'widget'));
         $product->addAttribute('color',array(
             'options'=>array('red','blue')
         ));
+
+        // save attribute
+        $attribute_mapper = new AttributeMapper($this->db);
+        $attribute_mapper->save($product->attribute('color'));
 
         // save product !
         $product_mapper = new ProductMapper($this->db);
         $id = $product_mapper->save($product);
 
         $loaded_product = $product_mapper->load($id);
-        $this->assertEquals(array('red','blue'), $loaded_product->attribute('color')->options(), 'should save attribute options');
+        $this->assertEquals(array('blue','red'), $loaded_product->attribute('color')->options(), 'should save attribute options');
     }
 
     function testShouldSaveConfigurableAttribute()
