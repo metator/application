2ee1a70489d8f28a5fd8682793017e50468d8a9b
josh <josh.ribakoff@gmail.com>
2013-05-12 21:33:48 -0400

    Cleaned up phpunit-bootstrap.php

--- phpunit-bootstrap.php
+++ phpunit-bootstrap.php
@@ -1,8 +1,7 @@
 <?php
-chdir('../');
 require_once 'vendor/autoload.php';
-defined('APPLICATION_PATH') || define('APPLICATION_PATH', __DIR__.'/application');
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', 'application');
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development');
 require_once 'Zend/Application.php';
-$application = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH.'/../config/application.ini');
+$application = new Zend_Application(APPLICATION_ENV, 'config/application.ini');
 $application->bootstrap();
