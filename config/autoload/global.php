3a1603dc05b20e3f0b35a58ae18e9f72b1ea67e5
josh <josh.ribakoff@gmail.com>
2013-06-02 16:43:35 -0400

    Create a default DB credential that works with default installation of mysql (travis)

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -18,10 +18,8 @@ return array(
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
-        'db' => array(
-            'username' => 'YOUR USERNAME HERE',
-            'password' => 'YOUR PASSWORD HERE',
-        ),
+        'username' => 'root',
+        'password' => '',
     ),
     'service_manager' => array(
         'factories' => array(
