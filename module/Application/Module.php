1ff809e78fcfa9888bfd818bd6fbee0a8e4f7a58
josh <josh.ribakoff@gmail.com>
2013-07-06 21:05:31 -0400

    Added 'test' field to register form (will modify this shortly), started adding some text to the checkout page as well as the zfcUser login widget

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -12,14 +12,34 @@ namespace Application;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 use Application\ProductMapper;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 
 class Module
 {
+    function init($moduleManager)
+    {
+        /** Make sure the ZfcUser is loaded previously, so we can override it later. */
+        $moduleManager->loadModule('ZfcUser');
+    }
+
     public function onBootstrap(MvcEvent $e)
     {
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+
+        /**
+         * Hook into ZfcUser & add new registration field(s)
+         */
+        $events = $e->getApplication()->getEventManager()->getSharedManager();
+        $events->attach('ZfcUser\Form\Register','init', function($e) {
+            $form = $e->getTarget();
+            $form->add(array(
+                'name'=>'Test',
+                'options'=>array('label'=>'Test')
+            ));
+        });
+
     }
 
     public function getConfig()
