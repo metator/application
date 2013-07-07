b850851448dfa179dcb827f612159327f04eb2ef
josh <josh.ribakoff@gmail.com>
2013-07-07 14:25:41 -0400

    Adding tabs / laying out product edit forms / added 'default' col to product_images

---
+++ data/migrations/20130707140658_create_default_product_image_flag.php
@@ -0,0 +1,38 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateDefaultProductImageFlag extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     */
+    public function change()
+    {
+        $this->table('product_images')
+            ->addColumn('default','integer',array('limit'=>1))
+            ->save();
+    }
+
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+    
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
