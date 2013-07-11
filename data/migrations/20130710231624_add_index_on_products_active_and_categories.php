0d1a56b1d181de9e32b114fb8864fca38963987e
josh <josh.ribakoff@gmail.com>
2013-07-10 23:18:07 -0400

    Add index on product 'active' column, and also on product_id in product_categories table.

---
+++ data/migrations/20130710231624_add_index_on_products_active_and_categories.php
@@ -0,0 +1,36 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddIndexOnProductsActiveAndCategories extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->execute('ALTER TABLE  `product` ADD INDEX (  `active` )');
+        $this->execute('ALTER TABLE  `product_categories` ADD INDEX (  `product_id` )');
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
