9813b2afe8ac134dbe4508b69e24e4d69b8752a8
josh <josh.ribakoff@gmail.com>
2013-08-11 14:09:55 -0400

    Split import test, moved 'category' related tests to their own test class

--- module/Product/test/Import/CategoryTest.php
+++ module/Product/test/Import/CategoryTest.php
@@ -8,7 +8,7 @@
 use Metator\Product\Importer;
 use Metator\Product\DataMapper as ProductDataMapper;
 
-class ImportTest extends PHPUnit_Framework_TestCase
+class Import_CategoryTest extends PHPUnit_Framework_TestCase
 {
     function setUp()
     {
@@ -29,74 +29,6 @@ class ImportTest extends PHPUnit_Framework_TestCase
         $this->db->query("delete from `product`", \Zend\Db\Adapter\Adapter::QUERY_MODE_EXECUTE);
         $this->db->query("delete from `category`", \Zend\Db\Adapter\Adapter::QUERY_MODE_EXECUTE);
         $this->db->query("delete from `product_categories`", \Zend\Db\Adapter\Adapter::QUERY_MODE_EXECUTE);
-    }
-
-    function testShouldImportSku()
-    {
-        $csv = "sku,name\n";
-        $csv.= "123,name";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $this->assertTrue($this->productExists('123'), 'should import product');
-    }
-
-    function testShouldImportDifferentOrder()
-    {
-        $csv = "name,sku\n";
-        $csv.= "name,123";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $this->assertTrue($this->productExists('123'), 'should import fields in any order');
-    }
-
-    function testShouldImportActiveFlag()
-    {
-        $csv = "sku,name,active\n";
-        $csv.= "123,name1,1\n";
-        $csv.= "456,name2,0";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $products = $this->productDataMapper()->find(['active'=>1]);
-        $this->assertEquals(1, count($products), 'should import active flag');
-    }
-
-    function testShouldImportName()
-    {
-        $csv = "sku,name\n";
-        $csv.= "123,name";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $this->assertEquals('name', $this->findProductBySku('123')->getName(), 'should import name');
-    }
-
-    function testShouldImportPrice()
-    {
-        $csv = "sku,name,base_price\n";
-        $csv.= "123,name,9.99";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $this->assertEquals(9.99, $this->findProductBySku('123')->getBasePrice(), 'should import price');
-    }
-
-    function testShouldImportAttributes()
-    {
-        $csv = "sku,name,base_price,attributes\n";
-        $csv.= '123,name,0,{"color":"red"}';
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $this->assertEquals('red', $this->findProductBySku('123')->attributeValue('color'), 'should import attribute');
     }
 
     function testShouldImportCategoryId()
@@ -170,77 +102,6 @@ class ImportTest extends PHPUnit_Framework_TestCase
         $categoryMapper = new \Metator\Category\DataMapper($this->db);
         $categories = $categoryMapper->findAll();
         $this->assertEquals(1, count($categories), 'should not create duplicate categories');
-    }
-
-    function testShouldImportMultipleProduct()
-    {
-        $csv = "sku,name\n";
-        $csv.= "123,name\n";
-        $csv.= "456,name";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $this->assertTrue($this->productExists('123'), 'should import product');
-    }
-
-    function testShouldNotReImportPreviousImports()
-    {
-        $csv = "sku,name,categories\n";
-        $csv.= "123,name,test\n";
-        $csv.= "456,name,test";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $csv = "sku,name,categories\n";
-        $csv.= "789,name,test2\n";
-        $csv.= "abc,name,test2";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $products = $this->productDataMapper()->find();
-
-        $this->assertEquals(4, count($products), 'should not re-import previous imports (should cleanup import table when done');
-    }
-
-    function testShouldNotChangeIDOfExistingSKUs()
-    {
-        $csv = "sku,name\n";
-        $csv.= "123,name";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $products1 = $this->productDataMapper()->find();
-
-        $csv = "sku,name\n";
-        $csv.= "123,name-new";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $products2 = $this->productDataMapper()->find();
-        $this->assertEquals($products1[0]->id(), $products2[0]->id(), 'should not change ID of existing products on re-import');
-    }
-
-    function testShouldUpdateNameOnReimport()
-    {
-        $csv = "sku,name\n";
-        $csv.= "123,name";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $csv = "sku,name\n";
-        $csv.= "123,name-new";
-
-        $importer = new Importer($this->db);
-        $importer->importFromText($csv);
-
-        $products = $this->productDataMapper()->find();
-        $this->assertEquals('name-new',$products[0]->getName(), 'should update values on re-import');
     }
 
     function productExists($sku)
